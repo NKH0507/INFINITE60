64acf410fa1cd2d3d63a5cbec0bcfa785e1e6518
woonge06 <woonge325@gmail.com>
2026-09-22 17:36:53 +0900

    패널 이동 및 공 튕기기 구현
    패널 이동 및 공 튕기기 및 벽 충돌 구현

--- INFINITE60/Assets/Scripts/Ball.cs
+++ INFINITE60/Assets/Scripts/Ball.cs
@@ -1,17 +1,97 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Ball : MonoBehaviour
+public class ball : MonoBehaviour
 {
-    public float speed = 5.0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool isTouchLeft;
+    public bool isTouchRight;
+    public bool isTouchUp;
+    public bool isTouchDown;
+
+    Rigidbody2D rigid;
+
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        // 테스트용 공 대각선 이동
+        rigid.linearVelocity = new Vector2(3, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ballmove();
     }
+
+    void ballmove()
+    {
+        Vector2 velocity = rigid.linearVelocity; // 현재 공 속도
+        float h = velocity.x;
+        float v = velocity.y;
+    }
+
+    // 충돌 플래그 생성 + 공 튕기기
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            switch (collision.gameObject.name)
+            {
+                // 왼쪽 벽에 부딪힘
+                case "leftwall":
+                    isTouchLeft = true;
+                    // X 방향 반전
+                    rigid.linearVelocity = new Vector2(-rigid.linearVelocity.x, rigid.linearVelocity.y);
+                    break;
+                // 오른쪽 벽에 부딪힘
+                case "rightwall":
+                    isTouchRight = true;
+                    // X 방향 반전
+                    rigid.linearVelocity = new Vector2(-rigid.linearVelocity.x, rigid.linearVelocity.y);
+                    break;
+                // 위쪽 벽에 부딪힘
+                case "upwall":
+                    isTouchUp = true;
+                    // Y 방향 반전
+                    rigid.linearVelocity = new Vector2(rigid.linearVelocity.x, -rigid.linearVelocity.y);
+                    break;
+                // 아래쪽 벽에 부딪힘
+                case "downwall":
+                    isTouchDown = true;
+                    // Y 방향 반전
+                    rigid.linearVelocity = new Vector2(rigid.linearVelocity.x, -rigid.linearVelocity.y);
+                    break;
+            }
+
+        }
+
+    }
+
+    // 플래그 지우기
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "leftwall":
+                    isTouchLeft = false;
+                    break;
+
+                case "rightwall":
+                    isTouchRight = false;
+                    break;
+
+                case "upwall":
+                    isTouchUp = false;
+                    break;
+
+                case "downwall":
+                    isTouchDown = false;
+                    break;
+            }
+
+        }
+
+    }
+
 }
